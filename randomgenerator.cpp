0358995df8837c6581354337f29b2f83cff1b526
MSPTF <69215950+MSPTF@users.noreply.github.com>
2022-04-15 20:46:42 +0200

    Update and rename README.md to randomgenerator.cpp

--- randomgenerator.cpp
+++ randomgenerator.cpp
@@ -2,8 +2,10 @@
 
 std::string random_string(const int len) {
     const std::string alpha_numeric("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890");
-    std::default_random_engine generator{ std::random_device{}() };
+//    
+    std::default_random_engine generator{ std::random_device{}() };  
     const std::uniform_int_distribution< std::string::size_type > distribution{ 0, alpha_numeric.size() - 1 };
+//    
     std::string str(len, 0);
     for (auto& it : str) {
         it = alpha_numeric[distribution(generator)];
@@ -14,3 +16,7 @@ std::string random_string(const int len) {
 - Main - !!
 
 SetConsoleTitle(random_string(20).c_str());
+
+// You no need this shit in your code
+
+#####################################################################################################################
